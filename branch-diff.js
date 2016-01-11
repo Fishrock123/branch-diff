f6208327d46c63a6a27f58f20460e1b721119a0d
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-01-11 13:51:43 -0500

    add --filter-release to filter out node-style releases

--- branch-diff.js
+++ branch-diff.js
@@ -159,6 +159,14 @@ if (require.main === module) {
     if (err)
       throw err
 
+    if (argv['filter-release']) {
+      list = list.filter((commit) => {
+        return !(/Working on v?\d\.\d\.\d/.test(commit.summary)
+              || /\d{4}-\d{2}-\d{2},? Version \d{1,2}\.\d{1,3}\.\d{1,3} ("[A-Za-z ]+" )?\((Stable|LTS)\)/.test(commit.summary)
+              || /\d{4}-\d{2}-\d{2},? io.js v\d{1,2}\.\d{1,3}\.\d{1,3} Release/.test(commit.summary))
+      })
+    }
+
     printCommits(list, simple)
   })
 }
